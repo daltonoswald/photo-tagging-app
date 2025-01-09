650d04862e8baae4d7902a482a2f874fe7de46de
daltonoswald <daltonoswald@gmail.com>
2025-01-09 13:38:38 -0500

    updated linkedin link

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
             <a href='https://www.theodinproject.com/lessons/nodejs-where-s-waldo-a-photo-tagging-app' className="footer-odin">
                 <img className="icon" src={odinIcon} alt="The Odin Project Icon"></img>
                 <div className="footer-text">
-                    <p>Where's Waldo &#40;A Photo Tagging App&#41;</p>
+                    <p>Where&apos;s Waldo &#40;A Photo Tagging App&#41;</p>
                     <p>From The Odin Project</p>
                 </div>
             </a>
@@ -18,7 +18,7 @@ export default function Footer() {
                     <a href='https://github.com/daltonoswald/photo-tagging-app' className="footer-github">
                         <img className="icon" src={githubIcon} alt="github icon" />
                     </a>
-                    <a href='https://linkedin.com/in/dalton-oswald-8aa955148' className="footer-linkedin">
+                    <a href='https://linkedin.com/in/daltonoswald' className="footer-linkedin">
                         <img className="icon" src={linkedinIcon} alt="linkedin icon" />
                     </a>
             </div>
