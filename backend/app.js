386adc1d40cc554cf4a89ebdad4bb1132cf16f49
daltonoswald <daltonoswald@gmail.com>
2025-01-14 11:51:12 -0500

    commented out views as they are not used

--- backend/app.js
+++ backend/app.js
@@ -33,8 +33,8 @@ app.use(cors({
 }))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 const index_controller = require('../controllers/index')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/leaderboard/:imageName', index_controller.score_get);
 
