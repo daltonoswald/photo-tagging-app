7499704dee6ca5f8618fc2ade17a17a5485a0eb6
daltonoswald <daltonoswald@gmail.com>
2025-01-25 12:39:58 -0500

    started target choosing

--- backend-prisma/controllers/indexController.js
+++ backend-prisma/controllers/indexController.js
@@ -1,6 +1,8 @@
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
-const { format } = require('date-fns')
+const { format } = require('date-fns');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 exports.index = asyncHandler(async (req, res) => {
     // res.render('index', { 
@@ -9,4 +11,27 @@ exports.index = asyncHandler(async (req, res) => {
     // })
     const testMessage = 'Successfully connected'
     res.json({testMessage})
+})
+
+exports.game_post = asyncHandler(async (req, res, next) => {
+    const {
+        imageName, targetChose, xPos, yPos
+    } = req.body;
+    console.log(imageName, targetChose, xPos, yPos);
+
+    const imagePlayed = await prisma.image.findFirst({
+        where: {
+            image_name: imageName
+        }
+    })
+    if (!imagePlayed) {
+        res.json({ error: 'Image not found' });
+    } else {
+        console.log(imagePlayed);
+        console.log('test', imagePlayed[targetChose+`_x_min`])
+        const minCoordinateX = imagePlayed[targetChose+`_x_min`]
+        const maxCoordinateX = imagePlayed[targetChose+`_x_max`]
+        const minCoordinateY = imagePlayed[targetChose+`_y_min`]
+        const maxCoordinateY = imagePlayed[targetChose+`_y_max`]
+    }
 })
--- backend-prisma/routes/indexRouter.js
+++ backend-prisma/routes/indexRouter.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get('/', indexController.index);
 
+router.post('/game', indexController.game_post);
+
 module.exports = router;
